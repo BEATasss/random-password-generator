274b75a4959c99cae40134f2919a6cfe6923a9c6
BEATasss <108656831+BEATasss@users.noreply.github.com>
2022-12-23 16:34:33 +0200

    copy button

--- passGen.js
+++ passGen.js
@@ -1,32 +1,19 @@
 var password = document.getElementById('password')
-var text_to_copy = document.getElementById("textcopy").innerHTML;
-    function genPassword() {
-        var chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        var passwordLength = 12
-        var password = ''
-        for (var i = 0; i <= passwordLength; i++) {
-            var randomNumber = Math.floor(Math.random() * chars.length)
-            password += chars.substring(randomNumber, randomNumber + 1)
-        }
-        document.getElementById('password').value = password
-    }
-    function copyPassword() {
-        var copyText = document.getElementById('password')
-        copyText.select()
-        copyText.setSelectionRange(0, 999)
-        // document.execCommand('copy')
-       
 
-if (!navigator.clipboard){
-    // use old commandExec() way
-} else{
-    navigator.clipboard.writeText(text_to_copy).then(
-        function(){
-            alert("yeah!"); // success 
-        })
-      .catch(
-         function() {
-            alert("err"); // error
-      });
-}  
+function genPassword() {
+    var chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var passwordLength = 12
+    var password = ''
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length)
+        password += chars.substring(randomNumber, randomNumber + 1)
     }
+    document.getElementById('password').value = password
+}
+function copyPassword() {
+    var copyText = document.getElementById('password')
+    copyText.select()
+    copyText.setSelectionRange(0, 9999)
+    document.execCommand('copy')
+    alert("Copied the text: " + copyText.value)
+}
